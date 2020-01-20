4acd2c8d8a0708c66a32167402ca25e0446bfc5c
Leo Shestakov <47545637+LeoShestakov@users.noreply.github.com>
2020-01-19 22:26:29 -0500

    Update browseplayer.c

--- browseplayer.c
+++ browseplayer.c
@@ -5,8 +5,14 @@
 int main(int argc, char ** argv){
 
     char url[128];
-    url = argv[1]
-	    
+
+    // accepts link from command line
+	fgets(url, 128, stdin);
+
+    // music links
+    // http://www.jsayles.com/music/weep.mp3
+    // http://www.jsayles.com/music/2015/What%20if%20I%20never%20speede%20-%20an%20Ayre%20by%20John%20Dowland.mp3
+
     char call[256];
     strcpy(call, "xdg-open "); // opens music in browser
     strcat(call, url); // append url

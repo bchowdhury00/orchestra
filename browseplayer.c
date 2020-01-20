08b2cd7099a4768163f9afa5e72288ae503c3daa
Leo Shestakov <47545637+LeoShestakov@users.noreply.github.com>
2020-01-19 22:25:12 -0500

    Update browseplayer.c

--- browseplayer.c
+++ browseplayer.c
@@ -5,12 +5,8 @@
 int main(int argc, char ** argv){
 
     char url[128];
-
-    // accepts link from command line
-	if (argc < 2)
-        perror("No bueno")
-        exit(0)
     url = argv[1]
+	    
     char call[256];
     strcpy(call, "xdg-open "); // opens music in browser
     strcat(call, url); // append url

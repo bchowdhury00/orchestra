062b94501239da0d6e35395da2acc6771f08034b
athompson00 <athompson00@stuy.edu>
2020-01-21 18:03:01 -0500

    final push

--- select_client.c
+++ select_client.c
@@ -6,14 +6,13 @@
 #include <string.h>
 #include <unistd.h>
 
-
 char * getLink(int n){
   FILE * f = fopen("links.txt", "r");
   int linknum = 0;
   char *input = malloc(200 * sizeof(char));
   while(fgets(input, 200 * sizeof(char), f)){
     if (n == linknum){
-	  fclose(f);
+	    fclose(f);
       return input;
     } else {
       linknum++;
@@ -31,11 +30,11 @@ int main(int argc, char **argv) {
         server_socket = client_setup( TEST_IP );
     int newbuffer = 2;
     while (1) {
-        //printf("enter data: ");
+        // printf("enter data: ");
         FD_ZERO(&read_fds);
-        FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
-        FD_SET(server_socket, &read_fds); //add socket to fd set
-        //select will block until either fd is ready
+        FD_SET(STDIN_FILENO, &read_fds); // add stdin to fd set
+        FD_SET(server_socket, &read_fds); // add socket to fd set
+        // select will block until either fd is ready
         select(server_socket + 1, &read_fds, NULL, NULL, NULL);
 
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
@@ -44,16 +43,16 @@ int main(int argc, char **argv) {
             write(server_socket, buffer, sizeof(buffer));
             read(server_socket, buffer, sizeof(buffer));
             printf("received: [%s]\n", buffer);
-        }//end stdin select
+        }// end stdin select
 
-        //currently the server is not set up to
-        //send messages to all the clients, but
-        //this would allow for broadcast messages
+        // currently the server is not set up to
+        // send messages to all the clients, but
+        // this would allow for broadcast messages
         if (FD_ISSET(server_socket, &read_fds)) {
             char * message = "send me a";
             write(server_socket,message,sizeof(message));
             recv(server_socket, buffer, sizeof(buffer),0);
-            printf("-----[%s]\n",buffer);
+            printf("--Length Received--[%s]\n",buffer);
             if (strlen(buffer) == 1) {
                 newbuffer = 0;
                 break;
@@ -75,18 +74,21 @@ int main(int argc, char **argv) {
                 break;
             }
         }
-    } //end while loop
+    } // end while loop
       // printf("[SERVER BROADCAST] [%c]\n", buffer);
       // printf("enter data: ");
-      //the above printf does not have \n
-      //flush the buffer to immediately print
-      //fflush(stdout);
-  //end socket select
+      // the above printf does not have \n
+      // flush the buffer to immediately print
+      // fflush(stdout);
+  // end socket select
   char *url;
   url = getLink(newbuffer);
   char call[256];
-  //strcpy(call, "xdg-open "); // for ubuntu
-  strcpy(call, "xdg-open "); // for mac
+
+  strcpy(call, "xdg-open "); // for ubuntu
+  //strcpy(call, "open "); // for mac
+
   strcat(call, url); // append url
+  free(url);
   system(call); // opens music in browser
 }

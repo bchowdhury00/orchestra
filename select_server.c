9cc858342d2f8ec5e37be5b8e76934a7646b6c3e
athompson00 <athompson00@stuy.edu>
2020-01-21 18:09:42 -0500

    Revert "final push"
    This reverts commit 062b94501239da0d6e35395da2acc6771f08034b.

--- select_server.c
+++ select_server.c
@@ -24,20 +24,20 @@ int main() {
     int f;
     int subserver_count = 0;
     char buffer[BUFFER_SIZE];
-    // set of file descriptors to read from
+    //set of file descriptors to read from
     fd_set read_fds;
     listen_socket = server_setup();
     while (1) {
-        // select() modifies read_fds
-        // we must reset it at each iteration
-        FD_ZERO(&read_fds); // 0 out fd set
-        FD_SET(STDIN_FILENO, &read_fds); // add stdin to fd set
-        FD_SET(listen_socket, &read_fds); // add socket to fd set
+        //select() modifies read_fds
+        //we must reset it at each iteration
+        FD_ZERO(&read_fds); //0 out fd set
+        FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
+        FD_SET(listen_socket, &read_fds); //add socket to fd set
 
-    // select will block until either fd is ready
+    //select will block until either fd is ready
     select(listen_socket + 1, &read_fds, NULL, NULL, NULL);
 
-    // if listen_socket triggered select
+    //if listen_socket triggered select
     if (FD_ISSET(listen_socket, &read_fds)) {
      client_socket = server_connect(listen_socket);
 
@@ -45,7 +45,7 @@ int main() {
      subserver_count++;
      if (f == 0){
      char * memory;
-       // subserver(client_socket);
+       //subserver(client_socket);
            if (subserver_count - 1 == 0)
                memory = "a";
            if (subserver_count - 1 == 1)
@@ -63,15 +63,15 @@ int main() {
        wait(NULL);
        close(client_socket);
      }
-    }// end listen_socket select
+    }//end listen_socket select
 
-    // if stdin triggered select
+    //if stdin triggered select
     if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-      // if you don't read from stdin, it will continue to trigger select()
+      //if you don't read from stdin, it will continue to trigger select()
       fgets(buffer, sizeof(buffer), stdin);
       printf("[server] subserver count: %d\n", subserver_count);
 
-    }// end stdin select
+    }//end stdin select
   }
 }
 

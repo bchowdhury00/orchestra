c6e932913431b82392e774cadea31e3e77ae1a10
bchowdhury00 <bchowdhury00@stuy.edu>
2020-01-20 23:37:15 -0500

    m

--- select_server.c
+++ select_server.c
@@ -87,10 +87,10 @@ void subserver(int client_socket) {
     semop(semd, &sb, 1);
     int shmd = shmget(KEY,SEG_SIZE,0);
     memory = shmat(shmd,0,0);
-    print(memory)
+    print("%d",*memory)
     char c = 48 + memory;
     write(client_socket,c,sizeof(c));
-    memory = memory + 1;
+    *memory = *memory + 1;
     shmdt(memory);
     sb.sem_op = 1;
     semop(semd, &sb, 1);

c85ad91200df56419cb78db14fdbfe401ba804db
bchowdhury00 <bchowdhury00@stuy.edu>
2020-01-20 22:28:46 -0500

    m

--- select_server.c
+++ select_server.c
@@ -54,13 +54,17 @@ void subserver(int client_socket) {
   char buffer[BUFFER_SIZE];
 
   //for testing client select statement
-  if (g < 5){
-      char c = 48 + g;
-      write(client_socket, c, sizeof(c));
-      g = g + 1;
-  }
+  //strncpy(buffer, , sizeof(buffer));
+  //write(client_socket, buffer, sizeof(buffer));
+
   while (read(client_socket, buffer, sizeof(buffer))) {
     printf("[subserver %d] received: [%s]\n", getpid(), buffer);
+    if (g < 5){
+        char c = 48 + g;
+        write(client_socket, c, sizeof(c));
+        printf("Sent [%d]%c",getpid(),c);
+        g = g + 1;
+    }
   }//end read loop
   close(client_socket);
   exit(0);

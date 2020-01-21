9e2e53442d60cf0fd2b3737900e0a6922f494bf6
athompson00 <athompson00@stuy.edu>
2020-01-21 15:01:40 -0500

    fixing memory

--- select_client.c
+++ select_client.c
@@ -22,78 +22,68 @@ char * getLink(int n){
 }
 
 int main(int argc, char **argv) {
+    int server_socket;
+    char buffer[BUFFER_SIZE];
+    fd_set read_fds;
+    if (argc == 2)
+        server_socket = client_setup( argv[1]);
+    else
+        server_socket = client_setup( TEST_IP );
+    int newbuffer;
+    while (1) {
+        printf("enter data: ");
+        fflush(stdout);
+        FD_ZERO(&read_fds);
+        FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
+        FD_SET(server_socket, &read_fds); //add socket to fd set
+        //select will block until either fd is ready
+        select(server_socket + 1, &read_fds, NULL, NULL, NULL);
 
-  int server_socket;
-  char buffer[BUFFER_SIZE];
+        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            fgets(buffer, sizeof(buffer), stdin);
+            *strchr(buffer, '\n') = 0;
+            write(server_socket, buffer, sizeof(buffer));
+            read(server_socket, buffer, sizeof(buffer));
+            printf("received: [%s]\n", buffer);
+        }//end stdin select
 
-  fd_set read_fds;
-
-  if (argc == 2)
-    server_socket = client_setup( argv[1]);
-  else
-    server_socket = client_setup( TEST_IP );
-
-  while (1) {
-
-    printf("enter data: ");
-    //the above printf does not have \n
-    //flush the buffer to immediately print
-    fflush(stdout);
-
-    //select() modifies read_fds
-    //we must reset it at each iteration
-    FD_ZERO(&read_fds);
-    FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
-    FD_SET(server_socket, &read_fds); //add socket to fd set
-
-    //select will block until either fd is ready
-    select(server_socket + 1, &read_fds, NULL, NULL, NULL);
-
-    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-      fgets(buffer, sizeof(buffer), stdin);
-      *strchr(buffer, '\n') = 0;
-      write(server_socket, buffer, sizeof(buffer));
-      read(server_socket, buffer, sizeof(buffer));
-      printf("received: [%s]\n", buffer);
-    }//end stdin select
-
-    //currently the server is not set up to
-    //send messages to all the clients, but
-    //this would allow for broadcast messages
-    if (FD_ISSET(server_socket, &read_fds)) {
-      recv(server_socket, buffer, sizeof(buffer),0);
-
-	  int newbuffer;
-	  if (strlen(buffer) == 1) {
-		  newbuffer = 0;
-	  }
-	  if (strlen(buffer) == 2) {
-		  newbuffer = 1;
-	  }
-	  if (strlen(buffer) == 3) {
-		  newbuffer = 2;
-	  }
-	  if (strlen(buffer) == 4) {
-		  newbuffer = 3;
-	  }
-	  if (strlen(buffer) == 5) {
-		  newbuffer = 4;
-	  }
-
-      char *url;
-      url = getLink(newbuffer);
-
-      char call[256];
-      strcpy(call, "xdg-open "); // opens music in browser
-      strcat(call, url); // append url
-      system(call);
-
-      printf("[SERVER BROADCAST] [%c]\n", buffer);
-      printf("enter data: ");
+        //currently the server is not set up to
+        //send messages to all the clients, but
+        //this would allow for broadcast messages
+        if (FD_ISSET(server_socket, &read_fds)) {
+            recv(server_socket, buffer, sizeof(buffer),0);
+	           if (strlen(buffer) == 1) {
+                   newbuffer = 0;
+                   break;
+               }
+               if (strlen(buffer) == 2) {
+                   newbuffer = 1;
+                   break;
+               }
+               if (strlen(buffer) == 3) {
+                   newbuffer = 2;
+                   break;
+               }
+               if (strlen(buffer) == 4) {
+                   newbuffer = 3;
+                   break;
+               }
+               if (strlen(buffer) == 5) {
+                   newbuffer = 4;
+                   break;
+	             }
+             }
+         } //end while loop
+      // printf("[SERVER BROADCAST] [%c]\n", buffer);
+      // printf("enter data: ");
       //the above printf does not have \n
       //flush the buffer to immediately print
-      fflush(stdout);
-    }//end socket select
-
-  }//end loop
+      //fflush(stdout);
+  //end socket select
+  char *url;
+  url = getLink(newbuffer);
+  char call[256];
+  strcpy(call, "xdg-open ");
+  strcat(call, url); // append url
+  system(call); // opens music in browser
 }
